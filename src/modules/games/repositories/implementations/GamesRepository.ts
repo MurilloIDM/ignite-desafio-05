dfc82fff27f54f7f340effc7d93cfbba886b4557
MurilloIDM <murillo.idem@gmail.com>
2021-09-13 08:10:10 -0300

    Implementação dos métodos nos repositórios de users e games

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -15,16 +15,27 @@ export class GamesRepository implements IGamesRepository {
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
       .createQueryBuilder()
+      .from(Game, "games")
+      .where("ILIKE games.title = :title", { title: param })
+      .getMany();
       // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`
+      SELECT count(*)
+      FROM "games"
+    `);
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
       .createQueryBuilder()
+      .from(User, "users")
+      .innerJoin("users.games", "games")
+      .where("games.id = :id", { id })
+      .getMany();
+
       // Complete usando query builder
   }
 }
